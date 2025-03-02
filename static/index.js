6257b5a0863ece6cb96c09f0c590ef69dd76c018
Rafael de Bem <debemrafael@gmail.com>
2025-03-02 17:48:39 -0300

    feat: add clerk/auth and update other css styles

--- static/index.js
+++ static/index.js
@@ -2,9 +2,33 @@ function getCurrentTheme() {
     return document.body.className;
 }
 
-window.onload = () => {
-    document.body.addEventListener("themeChanged", (evt) => {
-        console.log(evt.detail.value);
+async function configClerk() {
+    await Clerk.load();
+
+    const navbarRight = document.querySelector('.navbar-right');
+    const signInButton = document.getElementById('sign-in');
+    signInButton.className = 'title-wrapper';
+    signInButton.setAttribute('data-tooltip', Clerk.user ? 'Account settings' : 'Sign in');
+
+    const button = document.createElement('button');
+    button.innerHTML = Clerk.user ? '<i class="fas fa-user"></i>' : '<i class="fas fa-user-circle"></i>';
+    button.onclick = () => {
+        if (Clerk.user) {
+            Clerk.openUserProfile();
+        } else {
+            Clerk.openSignIn();
+        }
+    };
+
+    signInButton.innerHTML = '';
+    signInButton.appendChild(button);
+    navbarRight.insertBefore(signInButton, navbarRight.firstChild);
+}
+
+window.onload = async () => {
+    // await configClerk();
+
+    document.body.addEventListener("themeChanged", async (evt) => {
         document.body.className = evt.detail.value;
         const themeIconSpan = document.querySelector(".theme-icon");
         const syntaxThemeSelect = document.querySelector(".syntax-theme-select");
@@ -20,5 +44,35 @@ window.onload = () => {
         }
 
         htmx.trigger(themeIconSpan, "load");
+    });
+
+    document.body.addEventListener("htmx:afterSwap", function (evt) {
+        console.log("0", evt)
+
+        if (evt.detail.target.id === "post-content") {
+
+            console.log("1")
+
+            if (typeof MathJax !== "undefined" && MathJax.typesetPromise) {
+                console.log("2")
+                const content = evt.detail.target;
+                MathJax.typesetPromise([content])
+                    .then(() => {
+                        content.classList.remove("mathjax-ready");
+                        requestAnimationFrame(() => {
+                            content.classList.add("mathjax-ready");
+                        });
+                    })
+                    .catch((err) => {
+                        console.error("MathJax error:", err);
+                    });
+            }
+        }
+    });
+
+    document.body.addEventListener("htmx:beforeSwap", function (evt) {
+        if (evt.detail.target.id === "post-content") {
+            evt.detail.shouldSwap = true; // Ensure title updates
+        }
     });
 }
